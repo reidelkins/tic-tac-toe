0c9bce07116ce0137fda50a7f7a7a73b33997a19
reidelkins <reidelkins@outlook.com>
2024-04-15 01:22:46 -0500

    feat: initial commit for analytics, untested

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router'; // Import Router
 import { LoginComponent } from '../login/login.component';
+import { AnalyticsService } from '../core/services/analytics.service'; // Import AnalyticsService
 
 @Component({
   selector: 'app-header',
@@ -8,13 +9,21 @@ import { LoginComponent } from '../login/login.component';
   imports: [
     LoginComponent
   ],
+  providers: [
+    AnalyticsService
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
   constructor(    
-    private router: Router    
+    private router: Router,
+    private analyticsService: AnalyticsService
     ) {} 
+
+  ngOnInit() {
+    this.analyticsService.trackEvent('PAGE_LOADED', 'Header loaded', 'HEADER');
+  }
 
   goHome() {
     this.router.navigate(['/']);
